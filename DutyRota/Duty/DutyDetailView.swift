979b452d669bb033b82514914221d2452654c6e3
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2024-12-08 16:11:35 +0000

    Documentation for DutyDetailView

--- DutyRota/Duty/DutyDetailView.swift
+++ DutyRota/Duty/DutyDetailView.swift
@@ -9,24 +9,37 @@ import SwiftUI
 import UniformTypeIdentifiers
 import TipKit
 
+/// A view that shows details of duties of a duty period.
 struct DutyDetailView: View {
     @AppStorage("defaultColor") var defaultColor = "dutyGreen"
     @Environment(\.modelContext) var modelContext
+
+    /// A passed in duty period.
     @Bindable var duty: Duty
-
+    
+    /// A property to show add a duty detail sheet.
     @State private var showAddNewDuty = false
-
+    
+    /// A property to open a sheet to import a duty CSV file.
     @State private var isImporting = false
+
+    /// A property to export the duties details to a file CSV.
     @State private var isExporting: Bool = false
-
+    
+    /// A property to to filter the list of duty details.
     @State private var search = ""
-
+    
+    /// A property to show an alert if error when importing a duty file.
     @State private var showErrorImporting = false
-    @State private var message = ""
 
+    /// A property to to show the error message in an alert.
+    @State private var message = ""
+    
+    /// The header title of the list.
     let titles = ["Duty", "Start", "End", "Break", "ToD", "Spread"]
     let editDutyDetailTip = EditDutyDetailTip()
-
+    
+    /// The navigation title depends on when the dates of the period.
     var navigationTitle: String {
         let start = "\(duty.periodStart.formattedDayMonth)"
         var end = ""
@@ -37,11 +50,13 @@ struct DutyDetailView: View {
         }
         return start + " - " + end
     }
-
+    
+    /// A computed property to show instruction view on how to add duty details.
     var showFirstView: Bool {
         duty.unwrappedDutyDetails.count <= 3
     }
-
+    
+    /// A computed property of filtered duties depend on the `search`.
     var filteredDutyDetails: [DutyDetail] {
         if search.isNotEmpty {
             return duty.unwrappedDutyDetails.filter { $0.title.localizedCaseInsensitiveContains(search) }.sorted()
@@ -154,7 +169,9 @@ struct DutyDetailView: View {
             Text(message)
         }
     }
-
+    
+    /// A method to swipe to delete.
+    /// - Parameter indexSets: Index of the duty detail row.
     func deleteDutyDetail(_ indexSets: IndexSet) {
         for item in indexSets {
             let object = filteredDutyDetails[item]
@@ -162,7 +179,8 @@ struct DutyDetailView: View {
             duty.dutyDetails?.removeAll(where: { $0 == object })
         }
     }
-
+    
+    /// A method when first creating a new period added the 3 default duties which are needed to show correctly on calendar.
     func onAppearDefault() {
         guard duty.unwrappedDutyDetails.isEmpty else { return }
 
@@ -173,20 +191,27 @@ struct DutyDetailView: View {
         duty.dutyDetails?.append(restDetail)
         duty.dutyDetails?.append(spareDetail)
     }
-
+    
+    /// A method to sort and format a CSV file for import a duty file.
+    /// - Parameter file: The file name.
     func importDuties(of file: String) {
         var newFile = file
+
+        // this will change so header is not included.
         newFile = newFile.replacingOccurrences(of: "\r", with: "\n")
         newFile = newFile.replacingOccurrences(of: "\n\n", with: "\n")
 
+        // split file in to rows.
         var rows = newFile.components(separatedBy: "\n")
         rows.removeFirst()
 
         for row in rows {
             let columns = row.components(separatedBy: ",")
+            // make sure that the columns are the correct number either 4 or 5.
             guard columns.count >= 4 && columns.count <= 6 else { continue }
+            // ignore if the duties are the default duties.
             if columns[0] == "Spare" || columns[0] == "Rest" || columns[0] == "" { continue }
-
+            // if no color is present in import file then default color is used.
             let color: String
             if columns.count > 4 {
                 color = columns[4].trimmed
